ffe8e0b1c7858f718d92381cfcfaea344613fbb4
talha yay <394762@ogr.ktu.edu.tr>
2025-02-26 23:56:21 +0300

    color fixed
    color and sidebar parts has been done

--- index.js
+++ index.js
@@ -2,22 +2,21 @@ const { useState } = window.React;
 const { createRoot } = window.ReactDOM;
 
 function Discort() {
-  const [message, setMessage] = useState("Hoş geldin!");
+ const [message, setMessage] = useState("Hoş geldin!");
 
-  return (
-    <div className="app">
-      <h1>Discord-Like Front</h1>
-      <p>{message}</p>
-      <button onClick={() => setMessage("Merhaba, nasılsın?")}>
-        Mesaj Değiştir
-      </button>
-    </div>
-  );
+ // onClick={window.location.reload()}
+ return (
+  <div className="Discort">
+   <div className= "side">
+    <img src= "images\ds.png" alt="ds" className="side-ds"></img>
+   </div>
+
+   <div className= "sidebar"></div>
+  </div>
+ );
 }
 
-// index.html'de oluşturduğumuz #root elemanını bul
 const container = document.getElementById("root");
-// React 18 için createRoot API'si
 const root = createRoot(container);
 
 root.render(<Discort />);
